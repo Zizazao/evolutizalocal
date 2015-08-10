1691d0f5e69134afacc34294d16217d91ee39f03
zizazao <zizamv@gmail.com>
2015-08-10 16:49:01 +0200

    services styling
    added new pics, new custom css styles, added evo logo on header

--- resources/views/guest/welcome.blade.php
+++ resources/views/guest/welcome.blade.php
@@ -6,9 +6,9 @@
         <div class="tp-banner">
             <ul>
                 <!-- SLIDE -->
-                <li class="revolution-mch-1" data-transition="fade" data-slotamount="5" data-masterspeed="1000" data-title="Slide 1">
+                @foreach($lastSliders as $slider)
+                <li class="revolution-mch-1" data-transition="fade" data-slotamount="5" data-masterspeed="300" data-title="Slide 1">
                     <!-- MAIN IMAGE -->
-                    @foreach($lastSliders as $slider)
                     <img src="{{asset('postimages')}}/{{ $slider->img_name}}"  alt="darkblurbg"  data-bgfit="cover" data-bgposition="left top" data-bgrepeat="no-repeat">
 
                     <div class="tp-caption revolution-ch1 sft start"
@@ -20,9 +20,7 @@
                         data-easing="Back.easeInOut"
                         data-endeasing="Power1.easeIn"
                         data-endspeed="300">
-                        
-                            {!!$slider->title!!}
-                        @endforeach
+                        {!!$slider->title!!}
                     </div>
 
                     <!-- LAYER -->
@@ -37,9 +35,7 @@
                         data-endeasing="Power1.easeIn"
                         data-captionhidden="off"
                         style="z-index: 6">
-                        @foreach($lastSliders as $slider)
-                            {!!$slider->subtitle!!}<br>
-                        @endforeach
+                        {!!$slider->subtitle!!}<br>
                     </div>
 
                     <!-- LAYER -->
@@ -57,10 +53,11 @@
                         <a href="#" class="btn-u btn-brd btn-brd-hover btn-u-light">Leer más</a>
                     </div>
                 </li>
+                @endforeach
                 <!-- END SLIDE -->
 
                 <!-- SLIDE -->
-                <li class="revolution-mch-1" data-transition="fade" data-slotamount="5" data-masterspeed="1000" data-title="Slide 2">
+                <li class="revolution-mch-1" data-transition="fade" data-slotamount="5" data-masterspeed="300" data-title="Slide 2">
                     <!-- MAIN IMAGE -->
                     <img src="assets/img/sliders/2.jpg"  alt="darkblurbg"  data-bgfit="cover" data-bgposition="left top" data-bgrepeat="no-repeat">
 
@@ -111,7 +108,111 @@
                 <!-- END SLIDE -->
 
                 <!-- SLIDE -->
-                <li class="revolution-mch-1" data-transition="fade" data-slotamount="5" data-masterspeed="1000" data-title="Slide 3">
+
+                <li class="revolution-mch-1" data-transition="fade" data-slotamount="5" data-masterspeed="300" data-title="Slide 3">
+                    <!-- MAIN IMAGE -->
+                    <img src="assets/img/sliders/2.jpg"  alt="darkblurbg"  data-bgfit="cover" data-bgposition="left top" data-bgrepeat="no-repeat">
+
+                    <div class="tp-caption revolution-ch1 sft start"
+                        data-x="center"
+                        data-hoffset="0"
+                        data-y="100"
+                        data-speed="1500"
+                        data-start="500"
+                        data-easing="Back.easeInOut"
+                        data-endeasing="Power1.easeIn"
+                        data-endspeed="300">
+                        Includes 120+ Template Pages
+                    </div>
+
+                    <!-- LAYER -->
+                    <div class="tp-caption revolution-ch2 sft"
+                        data-x="center"
+                        data-hoffset="0"
+                        data-y="190"
+                        data-speed="1400"
+                        data-start="2000"
+                        data-easing="Power4.easeOut"
+                        data-endspeed="300"
+                        data-endeasing="Power1.easeIn"
+                        data-captionhidden="off"
+                        style="z-index: 6">
+                        We are creative technology company providing <br/>
+                        key digital services on web and mobile.
+                    </div>
+
+                    <!-- LAYER -->
+                    <div class="tp-caption sft"
+                        data-x="center"
+                        data-hoffset="0"
+                        data-y="310"
+                        data-speed="1600"
+                        data-start="1600"
+                        data-easing="Power4.easeOut"
+                        data-endspeed="300"
+                        data-endeasing="Power1.easeIn"
+                        data-captionhidden="off"
+                        style="z-index: 6">
+                        <a href="#" class="btn-u btn-brd btn-brd-hover btn-u-light">Learn More</a>
+                        <a href="#" class="btn-u btn-brd btn-brd-hover btn-u-light">Our Work</a>
+                    </div>
+                </li>
+                <!-- END SLIDE -->
+
+                <!-- SLIDE -->
+
+                <li class="revolution-mch-1" data-transition="fade" data-slotamount="5" data-masterspeed="1000" data-title="Slide 4">
+                    <!-- MAIN IMAGE -->
+                    <img src="assets/img/sliders/2.jpg"  alt="darkblurbg"  data-bgfit="cover" data-bgposition="left top" data-bgrepeat="no-repeat">
+
+                    <div class="tp-caption revolution-ch1 sft start"
+                        data-x="center"
+                        data-hoffset="0"
+                        data-y="100"
+                        data-speed="1500"
+                        data-start="1700"
+                        data-easing="Back.easeInOut"
+                        data-endeasing="Power1.easeIn"
+                        data-endspeed="300">
+                        Includes 120+ Template Pages
+                    </div>
+
+                    <!-- LAYER -->
+                    <div class="tp-caption revolution-ch2 sft"
+                        data-x="center"
+                        data-hoffset="0"
+                        data-y="190"
+                        data-speed="1400"
+                        data-start="2000"
+                        data-easing="Power4.easeOut"
+                        data-endspeed="300"
+                        data-endeasing="Power1.easeIn"
+                        data-captionhidden="off"
+                        style="z-index: 6">
+                        We are creative technology company providing <br/>
+                        key digital services on web and mobile.
+                    </div>
+
+                    <!-- LAYER -->
+                    <div class="tp-caption sft"
+                        data-x="center"
+                        data-hoffset="0"
+                        data-y="310"
+                        data-speed="1600"
+                        data-start="2800"
+                        data-easing="Power4.easeOut"
+                        data-endspeed="300"
+                        data-endeasing="Power1.easeIn"
+                        data-captionhidden="off"
+                        style="z-index: 6">
+                        <a href="#" class="btn-u btn-brd btn-brd-hover btn-u-light">Learn More</a>
+                        <a href="#" class="btn-u btn-brd btn-brd-hover btn-u-light">Our Work</a>
+                    </div>
+                </li>
+                <!-- END SLIDE -->
+
+                <!-- SLIDE -->
+                <li class="revolution-mch-1" data-transition="fade" data-slotamount="5" data-masterspeed="1000" data-title="Slide 5">
                     <!-- MAIN IMAGE -->
                     <img src="assets/img/sliders/3.jpg"  alt="darkblurbg"  data-bgfit="cover" data-bgposition="left top" data-bgrepeat="no-repeat">
 
